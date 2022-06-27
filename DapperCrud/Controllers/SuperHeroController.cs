0dc8e064d2b545b1dc0e4cd711e048e83d0c4430
Yusdenys Perez <yudemendoza@gmail.com>
2022-06-27 14:16:39 -0400

    Created CRUD with Dapper in a .NET 6 Web API using SQL Server

---
+++ DapperCrud/Controllers/SuperHeroController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DapperCrud.Models;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public SuperHeroController(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        /**
+         * GET Method
+         */
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHereos()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<SuperHero> heroes = await SelectAllHeroes(connection);
+            return Ok(heroes);
+        }
+
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("SELECT * FROM SuperHeroes WHERE Id = @Id"
+                , new { Id = heroId });
+            return Ok(heroes);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into superheroes (name,firstname,lastname,place) values (@Name,@FirstName,@LastName,@Place)", hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<SuperHero>> UpdateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update superheroes set name = @Name, firstname = @FirstName, lastname = @LastName, place = @Place where id = @Id", hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpDelete("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from superheroes where id = @Id", new { Id = heroId });
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("SELECT * FROM SuperHeroes");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Dapper tutorial
+CRUD with Dapper in a .NET 6 Web API using SQL Server
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [C# Dapper tutorial](https://zetcode.com/csharp/dapper/)
+* [Original code by Patrick God] (https://github.com/patrickgod/DapperCrudTutorial)
+
+
+
+
+
+
+
+
+
